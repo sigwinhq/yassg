b553135a71c79704b4d48c0011e2b95a1ee95b61
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2021-12-31 11:17:21 +0100

    YASSG project base dir injection

--- src/Bridge/Symfony/Kernel.php
+++ src/Bridge/Symfony/Kernel.php
@@ -5,6 +5,8 @@ namespace Sigwin\YASSG\Bridge\Symfony;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
 class Kernel extends \Symfony\Component\HttpKernel\Kernel
 {
@@ -22,5 +24,10 @@ class Kernel extends \Symfony\Component\HttpKernel\Kernel
     public function getCacheDir(): string
     {
         return $this->baseDir .'/cache';
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        $container->setParameter('yassg.base_dir', $this->baseDir);
     }
 }
