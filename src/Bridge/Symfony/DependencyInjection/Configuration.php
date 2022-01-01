ef83b4b946a64d2d3d670b66c0ba831de9738cab
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-01-01 16:09:25 +0100

    Config validation and merging

---
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sigwin\YASSG\Bridge\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('sigwin_yassg');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('routes')
+                    ->arrayPrototype()
+                        ->children()
+                            ->scalarNode('path')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->variableNode('catalog')
+                            ->end()
+                        ->end()
+                    ->end()
+                    ->requiresAtLeastOneElement()
+                    ->isRequired()
+                ->end()
+                ->variableNode('database')
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/Bridge/Symfony/DependencyInjection/KernelExtension.php
+++ src/Bridge/Symfony/DependencyInjection/KernelExtension.php
@@ -4,15 +4,18 @@ namespace Sigwin\YASSG\Bridge\Symfony\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Sigwin\YASSG\Bridge\Symfony\DependencyInjection\Configuration;
 
 class KernelExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        // TODO: validate config here
-        $container->setParameter('sigwin_yassg.database', $configs[0]['database']);
+        $configuration = new Configuration();
+
+        $config = $this->processConfiguration($configuration, $configs);
         
-        $container->setParameter('sigwin_yassg.routes', $configs[1]['routes']);
+        $container->setParameter('sigwin_yassg.database', $config['database']);
+        $container->setParameter('sigwin_yassg.routes', $config['routes']);
     }
     
     public function getNamespace(): string
