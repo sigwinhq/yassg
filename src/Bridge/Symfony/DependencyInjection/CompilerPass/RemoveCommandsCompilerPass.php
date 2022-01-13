8147039d241a0fb20f0e4ebb2e3ae84a6457450b
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-01-13 13:45:04 +0100

    feat: allow Symfony debug commands (#38)

--- src/Bridge/Symfony/DependencyInjection/CompilerPass/RemoveCommandsCompilerPass.php
+++ src/Bridge/Symfony/DependencyInjection/CompilerPass/RemoveCommandsCompilerPass.php
@@ -20,6 +20,8 @@ final class RemoveCommandsCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
+        $debug = (bool) $container->getParameter('kernel.debug');
+
         $commands = $container->findTaggedServiceIds('console.command');
         foreach (array_keys($commands) as $id) {
             $definition = $container->getDefinition($id);
@@ -29,7 +31,14 @@ final class RemoveCommandsCompilerPass implements CompilerPassInterface
                 continue;
             }
 
-            if (0 !== mb_strpos($className, 'Sigwin')) {
+            if (0 !== mb_strpos($className, 'Sigwin')
+                && ( ! $debug || ! \in_array($id, [
+                        'console.command.config_debug',
+                        'console.command.container_debug',
+                        'console.command.router_debug',
+                        'console.command.translation_debug',
+                        'twig.command.debug',
+                    ], true))) {
                 $container->removeDefinition($id);
             }
         }
