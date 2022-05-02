19d45a18260509e08f89f9bc90b3fba22f799c96
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-05-02 11:59:24 +0200

    infra: migrate to sigwin/infra, make prod dep

--- tests/functional/default.mk
+++ tests/functional/default.mk
@@ -1,9 +1,9 @@
 BUILD_DIR=public
 BASE_URL=https://example.com
 
-self/build: self/clean ${BUILD_DIR}/assets/entrypoints.json 
+self/build: self/clean ${BUILD_DIR}/assets/entrypoints.json
 	php ../../../bin/yassg yassg:generate --env prod $(BASE_URL) ${BUILD_OPTS}
-self/init: self/clean 
+self/init: self/clean
 	php ../../../bin/yassg yassg:init --demo
 self/clean:
 	rm -rf ${BUILD_DIR} var
@@ -12,4 +12,4 @@ self/check: self/build
 self/serve: self/build
 	php -S localhost:9999 -t ${BUILD_DIR}/
 
-include ../../../resources/Makefile
+include ../../../vendor/sigwin/infra/resources/YASSG/default.mk
--- tests/functional/site/Makefile
+++ tests/functional/site/Makefile
@@ -1,7 +1,5 @@
 .SILENT:
 
-APP_PORT := 9988
 APP_ROOT := $(abspath $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST))))))
-BUILD_DIR := public
 
-include vendor/sigwin/yassg/resources/Makefile
+include vendor/sigwin/infra/resources/YASSG/default.mk
