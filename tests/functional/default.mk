36d178b1c589005ba2388c8db8ed8fdc0f76d4d7
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-06-05 11:04:42 +0200

    test: add tests fixtures for an expected outcome

--- tests/functional/default.mk
+++ tests/functional/default.mk
@@ -11,7 +11,7 @@ self/init: self/clean ## Self: init the app via yassg:init
 self/clean:
 	rm -rf ${BUILD_DIR}
 self/test:
-	diff --recursive --color fixtures/ public/
+	sh -c "${PHPQA_DOCKER_COMMAND} diff -r fixtures/ public/"
 self/check: self/build ## Self: check the build
 	lychee --verbose --offline --base ./${BUILD_DIR} ./${BUILD_DIR}
 self/serve: self/build ## Self: serve the build
