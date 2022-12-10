e6ea75d24a499943a8d3fc130c2aa711ee397c27
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-12-10 16:45:04 +0100

    feat: route global variables

--- src/Bridge/Symfony/Kernel.php
+++ src/Bridge/Symfony/Kernel.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Sigwin\YASSG\Bridge\Symfony;
 
 use Sigwin\YASSG\Bridge\Symfony\DependencyInjection\CompilerPass\ConfigureDatabasesCompilerPass;
+use Sigwin\YASSG\Bridge\Symfony\DependencyInjection\CompilerPass\InjectRouteVariablesCompilerPass;
 use Sigwin\YASSG\Bridge\Symfony\DependencyInjection\CompilerPass\RemoveCommandsCompilerPass;
 use Sigwin\YASSG\Bridge\Symfony\DependencyInjection\KernelExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
@@ -57,6 +58,7 @@ final class Kernel extends \Symfony\Component\HttpKernel\Kernel
         $container->registerExtension(new KernelExtension());
         $container->addCompilerPass(new ConfigureDatabasesCompilerPass());
         $container->addCompilerPass(new RemoveCommandsCompilerPass());
+        $container->addCompilerPass(new InjectRouteVariablesCompilerPass());
     }
 
     public function registerBundles(): iterable
